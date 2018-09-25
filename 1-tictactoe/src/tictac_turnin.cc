f0380e232d4ef3532cb58bfdc6e848498ed76600
zackjohnson8 <zackjohnson8@gmail.com>
2018-09-25 13:12:14 -0700

    First Commit

---
+++ 1-tictactoe/src/tictac_turnin.cc
@@ -0,0 +1,46 @@
+#include <tictac_support.h>
+#include <stdio.h>
+/**
+	make_move: takes a board state and makes a legal 
+	(hopefully optimal) move
+
+	args:
+		int [][3] board: 3x3 array of ints representing the 
+		board state. The values of board are altered based
+		on the move
+			0: empty
+			1: x
+		 -1: o
+		
+	returns (int):
+		the number of steps it took to choose the best move
+		(current implementation returns 1 by default, 0 if no move made)
+**/
+
+int make_move( int board[][3] )
+{
+	// figure out what move it is
+	int state = 0;
+	for( int i = 0; i < 3; i++ )
+		for( int j = 0; j < 3; j++ )
+			state += board[i][j];
+
+	state = -state;
+	if( state == 0 )
+		state = 1;
+
+	// default behavior: find any unoccupied square and make the move
+	for( int i = 0; i < 3; i++ )
+		for( int j = 0; j < 3; j++ )
+			// find an empty square
+			if( board[i][j] == 0 )
+			{
+				// that's the move
+				printf( "player [%d] made move: [%d,%d]\n", state, i, j );
+				board[i][j] = state;
+				return 1;
+			}
+	
+	// no move was made (board was full)
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# cs482-fall2018
+Project Code For cs482/682 Fall 2018 @ UNR
+
+# project 1
+
+Create a tic-tac-toe move-maker program. I've given you some skeleton file I/O code that you can use and the spec for some functions that you have to fill in. Make those functions work, and you should get an 'A'
+
+## to compile
+
+```
+mkdir build && cd build
+cmake ..
+make
+```
